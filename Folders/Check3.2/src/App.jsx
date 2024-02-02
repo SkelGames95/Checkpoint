0e966f8ea7f016ec4c2c3f099e48d2bb5041d4a9
Marco.Polenta <polentamarco.dev@gmail.com>
2024-02-02 11:53:38 +0100

    Checkpoint3.2 setup

---
+++ Folders/Check3.2/src/App.jsx
@@ -0,0 +1,11 @@
+import { Comp } from "./Comp"
+
+export const App = () => {
+
+    return (
+        <div>
+            <h1>Checkpoint 3</h1>
+            
+        </div>
+    )
+}
---
+++ Folders/Check3.2/src/Comp.jsx
@@ -0,0 +1,10 @@
+
+
+export const Comp = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
